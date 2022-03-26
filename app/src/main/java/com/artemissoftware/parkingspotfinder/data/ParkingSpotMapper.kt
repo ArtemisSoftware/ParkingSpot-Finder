eb1940542b6157529c97c9b9d05c435661c3bec5
gustavo <gustavo.isaac@gmail.com>
2022-03-26 21:03:16 +0000

    ParkingSpotMapper

---
+++ app/src/main/java/com/artemissoftware/parkingspotfinder/data/ParkingSpotMapper.kt
@@ -0,0 +1,19 @@
+package com.artemissoftware.parkingspotfinder.data
+
+import com.artemissoftware.parkingspotfinder.domain.model.ParkingSpot
+
+fun ParkingSpotEntity.toParkingSpot(): ParkingSpot {
+    return ParkingSpot(
+        lat = lat,
+        lng = lng,
+        id = id
+    )
+}
+
+fun ParkingSpot.toParkingSpotEntity(): ParkingSpotEntity {
+    return ParkingSpotEntity(
+        lat = lat,
+        lng = lng,
+        id = id
+    )
+}
