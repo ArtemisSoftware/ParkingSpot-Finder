754a4afb4a498a9346441cfd6274078f3db7e89c
gustavo <gustavo.isaac@gmail.com>
2022-03-26 21:09:27 +0000

    AppModule

---
+++ app/src/main/java/com/artemissoftware/parkingspotfinder/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.artemissoftware.parkingspotfinder.di
+
+
+import android.app.Application
+import androidx.room.Room
+import com.artemissoftware.parkingspotfinder.data.ParkingSpotDatabase
+import com.artemissoftware.parkingspotfinder.data.ParkingSpotRepositoryImpl
+import com.artemissoftware.parkingspotfinder.domain.repository.ParkingSpotRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideParkingSpotDatabase(app: Application): ParkingSpotDatabase {
+        return Room.databaseBuilder(
+            app,
+            ParkingSpotDatabase::class.java,
+            "parking_spots.db"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideParkingSpotRepository(db: ParkingSpotDatabase): ParkingSpotRepository {
+        return ParkingSpotRepositoryImpl(db.dao)
+    }
+}
